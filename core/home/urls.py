0c4a256bb8292813c880d9c9d504e3dce639e497
AlveIbrahim <alnfibrahim@gmail.com>
2023-11-28 22:47:15 +0600

    done

--- core/home/urls.py
+++ core/home/urls.py
@@ -8,20 +8,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='home'),
-<<<<<<< HEAD
     path('home_after_login',views.home_after_login,name='home_after_login'),
-=======
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
     path('rs', views.rs, name='rs'),
     path('rent', views.rent, name='rent'),
     path('car_Listing', views.car_Listing, name='car_Listing'),
     path('signup', views.signup, name='signup'),
-<<<<<<< HEAD
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout')
-=======
-    path('login', views.login_view, name='login')
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- core/home/views.py
+++ core/home/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render, redirect, HttpResponse
 from home.models import car_listing
 from django.contrib.auth.forms import AuthenticationForm
-<<<<<<< HEAD
 from django.contrib import messages
 from .forms import SignupForm, cl
 from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate as auth_authenticate
-=======
-from .forms import SignupForm, cl
-from django.contrib.auth import login
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse('this is home page')
 
-<<<<<<< HEAD
 def home_after_login(request):
     return render(request,'after_login.html')
 
-=======
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
 def rs(request):
     return render(request, 'rideshare.html')
     #return HttpResponse('this is Ride Sharing page')
@@ -34,7 +26,7 @@ def car_Listing(request):
         c_l = cl(request.POST, request.FILES)
         if c_l.is_valid():
             c_l.save()
-            return redirect('home')
+            return redirect('home_after_login')
     else:
         c_l = cl()
     return render(request, 'car_listing.html', {'CL':c_l})
@@ -56,45 +48,28 @@ def signup(request):
         signup = SignupForm(request.POST)
         if signup.is_valid():
             user = signup.save()
-<<<<<<< HEAD
             auth_login(request, user)
             return redirect('home_after_login')
-=======
-            login(request, user)
-            return redirect('home')
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
         else:
             return redirect('signup')
     else:
         signup = SignupForm()
     return render(request, 'signup.html', {'Signup':signup})
 
-<<<<<<< HEAD
 def login(request):
-=======
-def login_view(request):
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
     if request.method == 'POST':
         login_form = AuthenticationForm(data=request.POST)
         if login_form.is_valid():
             user = login_form.get_user()
-<<<<<<< HEAD
             auth_login(request,user)
             return redirect('home_after_login')
-=======
-            login(request,user)
-            return redirect('home')
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
         else:
             return redirect('login')
     else:
         login_form = AuthenticationForm()
     return render(request, 'login.html', {'Login':login_form})
-<<<<<<< HEAD
 
 def logout(request):
     auth_logout(request)
     messages.warning(request, 'You were logged out')
     return redirect('home')
-=======
->>>>>>> 38c90125a8f265e08c0bc94a3a4b2b051824acaa
