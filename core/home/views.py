28c6f8cff92b4706a337978e65f043281bdfa71f
wackademia <151899221+wackademia@users.noreply.github.com>
2023-11-28 21:33:49 +0600

    Add files via upload

--- core/home/views.py
+++ core/home/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect, HttpResponse
 from home.models import car_listing
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from .forms import SignupForm, cl
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate as auth_authenticate
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse('this is home page')
+
+def home_after_login(request):
+    return render(request,'after_login.html')
 
 def rs(request):
     return render(request, 'rideshare.html')
@@ -44,23 +48,28 @@ def signup(request):
         signup = SignupForm(request.POST)
         if signup.is_valid():
             user = signup.save()
-            login(request, user)
-            return redirect('home')
+            auth_login(request, user)
+            return redirect('home_after_login')
         else:
             return redirect('signup')
     else:
         signup = SignupForm()
     return render(request, 'signup.html', {'Signup':signup})
 
-def login_view(request):
+def login(request):
     if request.method == 'POST':
         login_form = AuthenticationForm(data=request.POST)
         if login_form.is_valid():
             user = login_form.get_user()
-            login(request,user)
-            return redirect('home')
+            auth_login(request,user)
+            return redirect('home_after_login')
         else:
             return redirect('login')
     else:
         login_form = AuthenticationForm()
     return render(request, 'login.html', {'Login':login_form})
+
+def logout(request):
+    auth_logout(request)
+    messages.warning(request, 'You were logged out')
+    return redirect('home')
